815155ddd6dc6eea00571be0d21113ad7cf43352
dagger021 <samisamir19baranwal02@gmail.com>
2026-04-23 11:40:30 +0530

    add simple config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,13 @@
 package config
+
+import "time"
+
+const (
+	KafkaBroker  = "kafka:9092"
+	KafkaGroupID = "log-ingestor"
+
+	ClickhouseDSN = "clickhouse://@localhost:9000/default"
+
+	BatchSize     = 1000
+	FlushInterval = 2 * time.Second // flush batches in every two seconds, if batch is not full
+)
