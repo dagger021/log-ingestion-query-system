a6857840320642de1367a1fcabfb528b1915f2a4
dagger021 <samisamir19baranwal02@gmail.com>
2026-04-24 13:21:58 +0530

    fix LogEntryDB struct tag to 'ch'

--- internal/domain/log.go
+++ internal/domain/log.go
@@ -12,7 +12,7 @@ type Metadata struct {
 	ParentResourceID string `json:"parentResourceId"`
 }
 
-type baseLogEntry struct {
+type LogEntry struct {
 	ID         uuid.UUID `json:"id"`
 	Level      LogLevel  `json:"level"`
 	Message    string    `json:"message"`
@@ -21,27 +21,51 @@ type baseLogEntry struct {
 	TraceID    string    `json:"traceId"`
 	SpanID     string    `json:"spanId"`
 	Commit     string    `json:"commit"`
-}
 
-type LogEntry struct {
-	baseLogEntry
 	Metadata Metadata `json:"metadata"`
 }
 
 type LogEntryDB struct {
-	baseLogEntry
-	ParentResourceID string `json:"parentResourceId"`
+	ID               string    `json:"id" ch:"id"`
+	Level            string    `json:"level" ch:"level"`
+	Message          string    `json:"message" ch:"message"`
+	ResourceID       string    `json:"resourceId" ch:"resourceId"`
+	Timestamp        time.Time `json:"timestamp" ch:"timestamp"`
+	TraceID          string    `json:"traceId" ch:"traceId"`
+	SpanID           string    `json:"spanId" ch:"spanId"`
+	Commit           string    `json:"commit" ch:"commit"`
+	ParentResourceID string    `json:"parentResourceId" ch:"parentResourceId"`
 }
 
-func (l *LogEntryDB) ToDomain() LogEntry {
-	return LogEntry{
-		baseLogEntry: l.baseLogEntry,
-		Metadata:     Metadata{ParentResourceID: l.ParentResourceID},
+func (l *LogEntryDB) ToDomain() (*LogEntry, error) {
+	level, _ := ToLogLevel(l.Level)
+	id, err := uuid.Parse(l.ID)
+	if err != nil {
+		return nil, err
 	}
+	return &LogEntry{
+		ID:         id,
+		Level:      level,
+		Message:    l.Message,
+		ResourceID: l.ResourceID,
+		Timestamp:  l.Timestamp,
+		TraceID:    l.TraceID,
+		SpanID:     l.SpanID,
+		Commit:     l.Commit,
+		Metadata:   Metadata{ParentResourceID: l.ParentResourceID},
+	}, nil
 }
+
 func (l *LogEntry) ToDB() LogEntryDB {
 	return LogEntryDB{
-		baseLogEntry:     l.baseLogEntry,
+		ID:               l.ID.String(),
+		Level:            string(l.Level),
+		Message:          l.Message,
+		ResourceID:       l.ResourceID,
+		Timestamp:        l.Timestamp,
+		TraceID:          l.TraceID,
+		SpanID:           l.SpanID,
+		Commit:           l.Commit,
 		ParentResourceID: l.Metadata.ParentResourceID,
 	}
 }
@@ -61,7 +85,7 @@ func ParseLogFromJSON(data []byte) (*LogEntry, error) {
 	return &log, nil
 }
 
-func (l *baseLogEntry) Validate() error {
+func (l *LogEntry) Validate() error {
 	if _, ok := ToLogLevel(string(l.Level)); !ok {
 		return errors.New("invaild log level")
 	}
