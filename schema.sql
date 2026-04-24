c5524b61c624e0c4efbb45035bab433debd90087
dagger021 <samisamir19baranwal02@gmail.com>
2026-04-24 20:39:21 +0530

    add indexes in clickhouse schema

--- schema.sql
+++ schema.sql
@@ -8,8 +8,27 @@ CREATE TABLE IF NOT EXISTS logEntries (
     traceId String,
     spanId String,
     commit String,
-    parent_resourceId String,
+    parentResourceId String
 )
 ENGINE = ReplacingMergeTree()
 PARTITION BY toDate(timestamp)
 ORDER BY (timestamp, resourceId, traceId, spanId, id);
+
+-- ====================
+-- Add Indexes
+-- ====================
+ALTER TABLE logEntries DROP INDEX IF EXISTS idx_level;
+ALTER TABLE logEntries
+ADD INDEX idx_level level TYPE set(100) GRANULARITY 1;
+
+ALTER TABLE logEntries DROP INDEX IF EXISTS idx_resource;
+ALTER TABLE logEntries
+ADD INDEX idx_resource resourceId TYPE set(1000) GRANULARITY 1;
+
+ALTER TABLE logEntries DROP INDEX IF EXISTS idx_trace;
+ALTER TABLE logEntries
+ADD INDEX idx_trace traceId TYPE bloom_filter(0.01) GRANULARITY 1;
+
+ALTER TABLE logEntries DROP INDEX IF EXISTS idx_message;
+ALTER TABLE logEntries
+ADD INDEX idx_message message TYPE tokenbf_v1(1024, 3, 0) GRANULARITY 1;
