c2e0df1dacb505530ca49cb60e913fc0ff8b46b3
dagger021 <samisamir19baranwal02@gmail.com>
2026-04-24 20:38:14 +0530

    update migrate to avoid multi-line sql execution problem

--- internal/clickhouse/migrate.go
+++ internal/clickhouse/migrate.go
@@ -3,15 +3,26 @@ package clickhouse
 import (
 	"context"
 	"os"
+	"strings"
 
 	"github.com/ClickHouse/clickhouse-go/v2"
 )
 
 func Migrate(conn clickhouse.Conn, file string) error {
 	schema, err := os.ReadFile(file)
-	if err == nil {
-		err = conn.Exec(context.Background(), string(schema))
+	if err != nil {
+		return err
 	}
 
-	return err
+	// multi-line query is not allowed in clickhouse go driver
+	// divide sql file into separate queries
+	for query := range strings.SplitSeq(string(schema), ";") {
+		if query != "" {
+			if err := conn.Exec(context.Background(), query); err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
 }
